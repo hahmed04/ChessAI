c718bb7b685284e79715407fe3cb58f57d009aa9
natedoesthings <nathanaeldtesfaye@gmail.com>
2024-07-09 10:14:10 -0500

    added support for taking pieces, and added visuals for rest of pieces

--- ChessAI/GameLogic.swift
+++ ChessAI/GameLogic.swift
@@ -16,34 +16,74 @@ class ChessModel: ObservableObject {
     }
     
     func setupBoard() {
+        // Initialize pawns
         for i in 0...7 {
             board[1][i] = Pawn(position: (1, i), color: .white)
             board[6][i] = Pawn(position: (6, i), color: .black)
         }
+        
+        // Initialize knights
+        board[0][1] = Knight(position: (0, 1), color: .white)
+        board[0][6] = Knight(position: (0, 6), color: .white)
+        board[7][1] = Knight(position: (7, 1), color: .black)
+        board[7][6] = Knight(position: (7, 6), color: .black)
+        
+        // Initialize rooks
+        board[0][0] = Rook(position: (0, 0), color: .white)
+        board[0][7] = Rook(position: (0, 7), color: .white)
+        board[7][0] = Rook(position: (7, 0), color: .black)
+        board[7][7] = Rook(position: (7, 7), color: .black)
+        
+        // Initialize bishops
+        board[0][2] = Bishop(position: (0, 2), color: .white)
+        board[0][5] = Bishop(position: (0, 5), color: .white)
+        board[7][2] = Bishop(position: (7, 2), color: .black)
+        board[7][5] = Bishop(position: (7, 5), color: .black)
+        
+        // Initialize queens
+        board[0][3] = Queen(position: (0, 3), color: .white)
+        board[7][3] = Queen(position: (7, 3), color: .black)
+        
+        // Initialize kings
+        board[0][4] = King(position: (0, 4), color: .white)
+        board[7][4] = King(position: (7, 4), color: .black)
     }
     
     func handleTap(row: Int, column: Int) {
         if let piece = board[row][column] {
             if piece.color == activePlayer {
+                // Select or move the piece if the current player is tapping their own piece
                 if let selectedPiece = selectedPiece {
                     if selectedPiece.availableMoves(in: board).contains(where: { $0 == (row, column) }) {
+                        // Move the selected piece to the new position
                         movePiece(selectedPiece, to: (row, column))
                         activePlayer = activePlayer == .white ? .black : .white
                     } else {
+                        // Deselect if the move is not valid
                         self.selectedPiece = nil
                     }
                 } else {
+                    // Select the piece
                     self.selectedPiece = piece
                 }
-            }
-        } else if let selectedPiece = selectedPiece {
-            if selectedPiece.availableMoves(in: board).contains(where: { $0 == (row, column) }) {
+            } else if let selectedPiece = selectedPiece, selectedPiece.availableMoves(in: board).contains(where: { $0 == (row, column) }) {
+                // Capturing an opponent's piece
+                capturePiece(at: (row, column))
                 movePiece(selectedPiece, to: (row, column))
                 activePlayer = activePlayer == .white ? .black : .white
-            } else {
-                self.selectedPiece = nil
             }
+        } else if let selectedPiece = selectedPiece, selectedPiece.availableMoves(in: board).contains(where: { $0 == (row, column) }) {
+            // Move to an empty square
+            movePiece(selectedPiece, to: (row, column))
+            activePlayer = activePlayer == .white ? .black : .white
+        } else {
+            // Deselect the piece
+            self.selectedPiece = nil
         }
+    }
+
+    func capturePiece(at position: (Int, Int)) {
+        board[position.0][position.1] = nil
     }
 
     func movePiece(_ piece: ChessPiece, to position: (Int, Int)) {
---
+++ ChessAI/Pieces/Rook.swift
@@ -0,0 +1,40 @@
+//
+//  Queen.swift
+//  ChessAI
+//
+//  Created by Nathanael Tesfaye on 7/9/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Rook: ChessPiece {
+    var position: (Int, Int)  // Stores the current position of the knight on the board
+    let color: Player  // Represents the color of the knight, either black or white
+    var image: Image {
+        // Provides an image representation of the knight, selecting the correct asset based on color
+        color == .white ? Image("white_rook") : Image("black_rook")
+    }
+    
+    
+    func availableMoves(in board: [[ChessPiece?]]) -> [(Int, Int)] {
+        return [(0,0)]
+    }
+    
+    
+    // Updates the position of the knight to a new position
+    mutating func move(to newPosition: (Int, Int)) {
+        self.position = newPosition
+    }
+
+    // Helper function to check if a position is within the 8x8 chessboard
+    private func isValidPosition(_ position: (Int, Int)) -> Bool {
+        return position.0 >= 0 && position.0 < 8 && position.1 >= 0 && position.1 < 8
+    }
+    
+    
+    
+}
+    
+    
+    
