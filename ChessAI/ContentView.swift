73b55b31818370dc1a257a9dfe084f3d9329fea2
natedoesthings <nathanaeldtesfaye@gmail.com>
2024-07-08 21:37:34 -0500

    added board

--- ChessAI/ContentView.swift
+++ ChessAI/ContentView.swift
@@ -1,21 +1,29 @@
-//
-//  ContentView.swift
-//  ChessAI
-//
-//  Created by Nathanael Tesfaye on 7/8/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .stroke(Color.black, lineWidth: 5)
+                .frame(width: 320, height: 320) // 8 * 40 (since each cell is 40x40)
+            
+            VStack(spacing: 0) {
+                ForEach(0..<8) { row in
+                    HStack(spacing: 0) {
+                        ForEach(0..<8) { column in
+                            Button(action: {
+                                // Handle button tap
+                            }) {
+                                Rectangle()
+                                    .fill((row + column) % 2 == 0 ? Color.white : Color.black)
+                                    .frame(width: 40, height: 40)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .padding(.bottom)
     }
 }
 
