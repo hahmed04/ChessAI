9611580e5e7488eecf5ca5c3faa5be986bcb924e
natedoesthings <nathanaeldtesfaye@gmail.com>
2024-07-08 20:35:46 -0500

    Initial Commit

---
+++ ChessAI/ChessAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessAIApp.swift
+//  ChessAI
+//
+//  Created by Nathanael Tesfaye on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
